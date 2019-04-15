2578e0160d8cd3a6476dcbe0a142a9acad7cad86
Freerk van Zeijl <freerk.vanzeijl@ultrabold.com>
2019-04-15 17:15:36 +0200

    Updated Voucher-Model

--- src/Models/Voucher.php
+++ src/Models/Voucher.php
@@ -5,7 +5,22 @@ namespace Vanderb\LaravelShoppette\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Voucher extends Model{
-    
-    
-    
+
+    protected $fillable = [
+        'name',
+        'discount_amount',
+        'discount_percentage',
+        'qty',
+        'expired_on',
+        'active'
+    ];
+
+    protected $dates = [
+        'expired_on'
+    ];
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
+
 }
